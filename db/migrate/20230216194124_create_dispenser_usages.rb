1af643f72da7e9a84baf0141b9a2ac6441fff5f2
Adrian <adrigalileo@gmail.com>
2023-02-16 22:58:51 +0100

    Create dispenser a dispenser usages migrations

---
+++ db/migrate/20230216194124_create_dispenser_usages.rb
@@ -0,0 +1,12 @@
+class CreateDispenserUsages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :dispenser_usages do |t|
+      t.references :dispenser, null: false, foreign_key: true
+      t.datetime :opened_at, null: false
+      t.datetime :closed_at
+      t.float :total_spend
+
+      t.timestamps
+    end
+  end
+end
